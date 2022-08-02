49fa9bce02690ba3be67976287407b72d8775062
Larissa Portillo <larissaportillo10@gmail.com>
2022-08-02 19:36:20 -0400

    space fix, pokemon.js edits

---
+++ pokemon-app/server.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const mongoose = require('mongoose');
+
+const app = express();
+
+require('dotenv').config();
+const port = process.env.PORT || 3003;
+
+//connect mongoose database
+mongoose.connect(process.env.MONGO_URI, {useNewURLParser : true, useUnifiedTopology : true});
+mongoose.connection.once('open',()=>{
+    console.log('Connected to mongo');
+});
+
+//set views
+app.set('view engine','jsx');
+
+//initialize engine
+app.engine('jsx',require('express-react-views').createEngine());
+
+//data
+const Pokemon = require("./models/Pokemon");
+
+//routes
+
+
+//port
+app.listen(port,()=>{
+    console.log('Listening on port ', port);
+});
